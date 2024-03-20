d4860331856183cf3d867e8754a52104fb35c466
Vlad <vladtarasevich244@gmail.com>
2024-03-20 23:55:49 +0300

    return training

---
+++ src/exersices/returnTrng.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function mod(a, b) {
+    return a % b;
+}
+
+function exponent(a, b) {
+    return Math.pow(a, b);
+}
+
+function subt(a, b) {
+    return a - b;
+}
+
+
+console.log(add(5, 3));
+console.log(multiply(5, 3));
+console.log(divide(10, 2));
+console.log(mod(10, 3));
+console.log(exponent(2, 3));
+console.log(subt(10, 3));
